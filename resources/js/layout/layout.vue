796f6a1e2a2293d00162286ef59592cdc7bb5407
MeongGanas <farreluken@gmail.com>
2023-10-28 23:25:29 +0800

    menyambungkan antar halaman

--- resources/js/layout/layout.vue
+++ resources/js/layout/layout.vue
@@ -8,9 +8,16 @@
             </template>
 
             <v-app-bar-title>
-                <h1 class="text-xl font-semibold text-[#2C6BAC]">Present</h1>
+                <div class="flex items-center">
+                    <h1 class="text-xl font-semibold text-[#2C6BAC]">
+                        Present
+                    </h1>
+                    <span class="mx-2"
+                        ><v-icon icon="mdi:mdi-chevron-right"></v-icon
+                    ></span>
+                    <h1 class="text-lg font-semibold">{{ page }}</h1>
+                </div>
             </v-app-bar-title>
-
             <template v-slot:append>
                 <v-btn icon="mdi:mdi-plus"></v-btn>
                 <v-btn icon>
@@ -19,7 +26,12 @@
             </template>
         </v-app-bar>
         <v-navigation-drawer :width="220" v-model="drawer" class="py-3">
-            <v-list-item link title="Home" class="mb-3 px-6">
+            <v-list-item
+                link
+                @click="goToRoute('/home')"
+                title="Home"
+                class="mb-3 px-6"
+            >
                 <template v-slot:prepend>
                     <img
                         src="storage/icons/home.svg"
@@ -43,7 +55,12 @@
                 style="border-bottom: 1px solid gray"
                 class="mb-3"
             ></v-divider>
-            <v-list-item link title="Lists" class="px-6 mb-3">
+            <v-list-item
+                link
+                @click="goToRoute('/lists')"
+                title="Lists"
+                class="px-6 mb-3"
+            >
                 <template v-slot:prepend>
                     <img
                         src="storage/icons/list.svg"
@@ -60,7 +77,12 @@
                 class="mb-3"
                 style="border-bottom: 1px solid gray"
             ></v-divider>
-            <v-list-item link title="Settings" class="px-6">
+            <v-list-item
+                link
+                @click="goToRoute('/setting')"
+                title="Settings"
+                class="px-6"
+            >
                 <template v-slot:prepend>
                     <img
                         src="storage/icons/setting.svg"
@@ -82,7 +104,42 @@
     </v-app>
 </template>
 <script>
+import { usePage } from "@inertiajs/vue3";
+import { router } from "@inertiajs/vue3";
+import { ref, onMounted, watchEffect } from "vue";
+
 export default {
+    setup() {
+        function goToRoute(route) {
+            window.location.pathname = route;
+        }
+
+        const page = ref("Home");
+        const pageName = ref(page);
+
+        onMounted(() => {
+            // Set initial page value based on the current URL
+            updatePageFromUrl();
+        });
+
+        watchEffect(() => {
+            // Watch for changes in the URL and update the page accordingly
+            updatePageFromUrl();
+        });
+
+        function updatePageFromUrl() {
+            const route = window.location.pathname;
+            if (route === "/home") {
+                pageName.value = "Home";
+            } else if (route === "/lists") {
+                pageName.value = "Lists";
+            } else if (route === "/setting") {
+                pageName.value = "Settings";
+            }
+        }
+
+        return { page, goToRoute };
+    },
     data() {
         return {
             drawer: null,
